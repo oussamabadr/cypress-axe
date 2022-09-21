f0edda79c841d823690977dd587fba4a254c905f
oussamabadr <oussamabadr@gmail.com>
2022-09-21 05:01:08 +0200

    Change version for private use

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
 	e2e: {
-		baseUrl: 'http://localhost:8080',
+		baseUrl: 'https://example.cypress.io/',
 		video: false,
 	},
 });
